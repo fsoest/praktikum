665ffc8ee0d76f2df039cb44521878e89e7af002
Felix Soest <felix@soest-net.de>
2019-04-17 13:58:06 +0200

    v1.0

--- fehler.py
+++ fehler.py
@@ -1,48 +1,26 @@
 import sympy as sym
 
 
-# Messgroessen: 'Variable': (Wert, stat_Fehler, syst_Fehler)
-messgroessen = {
-    'h_107': (0.5206033574481798, 0.009143772011737834, 0),
-    'h_109': (0.4793966425518203, 0.009143772011737834, 0),
-    'Z_108_m': (7328.748180429714, 135.36197889253683, 0),
-    'Z_108_r': (7967.85020886897, 147.7239428724307, 0),
-    'Z_110_m': (36346.38397421291, 1060.2240205277908, 0),
-    'Z_110_r': (40967.32185647546, 1294.5673706646246, 0),
-}
-
-
-# Variableninitialisierung
-for key in messgroessen.keys():
-    exec("{0} = sym.Symbol('{0}')".format(key))
-
-Ag = h_107 * (Z_108_m / Z_108_r) + h_109 * (Z_110_m / Z_110_r)
-
-def fehler_berechnen(VarName, function, messgroessen):
+def fehler_berechnen(VarName, function_str, messgroessen):
     syst_fehler = 0
     stat_fehler = 0
     subs = []
 
+    # Variableninitialisierung
     for key in messgroessen.keys():
+        exec("{0} = sym.Symbol('{0}')".format(key))
         subs.append((key, messgroessen.get(key)[0]))
 
+    function = eval(function_str)
     Value = function.subs(subs)
 
     for val in messgroessen.keys():
         var = sym.Symbol(val)
-        # print(var)
-        # print(sym.diff(function, var))
         deriv = sym.diff(function, var).subs(subs)
-        # print(deriv)
-        # print(messgroessen.get(val)[1])
-        # print()
-        # print(messgroessen.get(key)[1]**2)
         stat_fehler += (deriv * messgroessen.get(val)[1])**2
         syst_fehler += (deriv * messgroessen.get(val)[2])**2
-        # print(stat_fehler)
     syst_fehler = sym.sqrt(syst_fehler)
     stat_fehler = sym.sqrt(stat_fehler)
-
 
     fehler = {
         'Name': VarName,
@@ -52,6 +30,3 @@ def fehler_berechnen(VarName, function, messgroessen):
     }
 
     return fehler
-
-
-print(fehler_berechnen("Silbergehalt", Ag, messgroessen))
---
+++ fehlertest.py
@@ -0,0 +1,19 @@
+import sympy as sym
+from fehler import fehler_berechnen
+
+# Messgroessen: 'Variable': (Wert, stat_Fehler, syst_Fehler)
+messgroessen = {
+    'h_107': (0.5206033574481798, 0.009143772011737834, 0),
+    'h_109': (0.4793966425518203, 0.009143772011737834, 0),
+    'Z_108_m': (7328.748180429714, 135.36197889253683, 0),
+    'Z_108_r': (7967.85020886897, 147.7239428724307, 0),
+    'Z_110_m': (36346.38397421291, 1060.2240205277908, 0),
+    'Z_110_r': (40967.32185647546, 1294.5673706646246, 0),
+}
+
+#var_init(messgroessen)
+
+Ag = "h_107 * (Z_108_m / Z_108_r) + h_109 * (Z_110_m / Z_110_r)"
+#var_init(messgroessen, ag)
+
+print(fehler_berechnen("Silbergehalt", Ag, messgroessen))
